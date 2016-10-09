50f205d99e56afcb0dd9bf9df0870f242b7d0531
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-10-08 23:38:55 -0500

    cool ass shit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp       = require('gulp');
 var livereload = require('gulp-livereload');
+var rev        = require('gulp-rev-append');
 
 // TASKS FOR LIVE RELOADING ==========
 gulp.task('css', function() {
@@ -14,13 +15,19 @@ gulp.task('html', function() {
   gulp.src(['*.html', 'views/*.html']).pipe(livereload())
 });
 
+gulp.task('rev', function() {
+  gulp.src('./index.html')
+    .pipe(rev())
+    .pipe(gulp.dest('.'));
+});
+
 // WATCH TASK =========================
 gulp.task('watch', function() {
   livereload.listen();
 
   gulp.watch(['*.html', 'views/*.html'], ['html']);
-  gulp.watch('css/*.css', ['css']);
-  gulp.watch('js/*.js', ['js']);
+  gulp.watch('css/*.css', ['css', 'rev']);
+  gulp.watch('js/*.js', ['js', 'rev']);
 });
 
 
